8e59170337309da3ba5492072d1cd629ae89f68d
Mihail Rkitcky <papi.pay@mail.ru>
2023-03-16 21:14:01 +0300

    Задачи № 17,19

--- task019.py
+++ task019.py
@@ -2,3 +2,17 @@
 # Необходимо сдвинуть всю последовательность
 # (сдвиг - циклический) на K элементов вправо, K – положительное число.
 
+input_list = []
+list_len = int(input("Введите кол-во элементов списка: "))
+for _ in range(list_len):
+    input_list.append(int(input("Введите число: ")))
+print(input_list)
+
+input_k = int(input("Введите число K: "))
+
+while input_k > len(input_list):
+    input_k = input_k - len(input_list)
+
+input_list = input_list[-input_k:] + input_list[:-input_k]
+
+print(input_list)
