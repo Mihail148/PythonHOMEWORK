1aef223882285417bd10ca7a330b9f9a16eb4a99
Mihail Rkitcky <papi.pay@mail.ru>
2023-03-16 21:37:39 +0300

    Задачи №17, 19, 21, 23

--- task023.py
+++ task023.py
@@ -2,3 +2,14 @@
 # Напишите программу, которая подсчитает количество элементов массива, 
 # больших предыдущего (элемента с предыдущим номером).
 
+input_list = []
+list_len = int(input("Введите кол-во элементов списка: "))
+for _ in range(list_len):
+    input_list.append(int(input("Введите число: ")))
+print(input_list)
+
+count = 0
+for i in range(list_len - 1):
+    if  input_list[i] < input_list[i + 1]:
+        count += 1
+print(f"Количество элементов массива, больших предыдущего: {count}")        
